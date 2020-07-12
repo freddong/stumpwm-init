07d46efbb2c85b32e88992e1284cb979be15f8b6
freddong <dcong64@gmail.com>
2020-07-12 11:56:38 +0800

    delay for 1 sec after the launch of fcitx.

--- finalize.lisp
+++ finalize.lisp
@@ -2,6 +2,10 @@
 
 (setf *mouse-focus-policy* :click)
 
+;; Electron apps (specifically Discord and Slack) receiving notifications causes windows to not appear https://github.com/i3/i3/issues/3130
+(run-shell-command "dunst")
+
+(run-shell-command "fcitx")
 (run-shell-command "xsetroot -cursor_name left_ptr -solid black -name root-window")
 
 ;;(run-shell-command "~/.screenlayout/main.sh")
@@ -14,9 +18,9 @@
 
 (run-shell-command "redshift-gtk")
 (run-shell-command "gtk-launch dropbox")
+
+;; 等待fcitx启动
+(sleep 1)
+
+;; last
 (stumpwm:run-commands "terminal" "firefox" "slack" "emacs")
-
-;; Electron apps (specifically Discord and Slack) receiving notifications causes windows to not appear https://github.com/i3/i3/issues/3130
-(run-shell-command "dunst")
-
-(run-shell-command "fcitx-autostart")
